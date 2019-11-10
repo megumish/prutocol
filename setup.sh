c0e1c83969f530b468f524219cd1beffd04c1ab5
megumish <megumish@exploitouri.st>
2019-11-10 07:45:30 +0000

    init the project

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# ---- start setup rust ----
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+. $HOME/.cargo/env
+echo ". $HOME/.cargo/env" >> $HOME/.bashrc
+
+rustup component add rls rust-analysis rust-src
+
+# ---- end setup rust ----
+# ---- start build project ----
+
+cargo build
+cargo test
+
+# ---- end build project ----
